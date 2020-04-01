14fb166508d944fe3f84944a0509bf8bcd7a9f0f
Freddie Hoy <freddiehoy@hotmail.com>
2020-04-01 13:06:25 +0100

    Add eslint and prettier format on save

--- src/common/Navbar.js
+++ src/common/Navbar.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const NameTitle = styled.h1`
   font-size: 100px;
-`
+`;
 
 export const Navbar = () => (
   <div>
     <NameTitle>Freddie Hoy</NameTitle>
   </div>
-)
+);
